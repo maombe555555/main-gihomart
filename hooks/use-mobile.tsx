48844ec79f8411c5b07f8406277764b7351a199d
sando <maseemmy200@gmail.com>
2025-11-26 18:30:40 +0200

    hooks modification

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -3,17 +3,28 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    // Only run on client side
+    if (typeof window === "undefined") return
+
+    const mediaQuery = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    
+    // Set the initial value
+    setIsMobile(mediaQuery.matches)
+
+    // Create event listener
+    const handler = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    // Add the listener
+    mediaQuery.addEventListener("change", handler)
+
+    // Clean up
+    return () => mediaQuery.removeEventListener("change", handler)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
